7863e3e874075f5213f1fdb74de237c7004aa3f9
Deepak Goel <goel.deepak9@gmail.com>
2018-07-31 01:39:28 -0400

    Textblob working with tweets, not using CSV storage right now need to implement later

--- tweetanalysis.py
+++ tweetanalysis.py
@@ -1,7 +1,7 @@
 import tweepy
 from constants import *
 import csv
-import unicodedata
+from textblob import TextBlob
 
 
 #Authentication using API keys
@@ -21,21 +21,38 @@ all_no_rts = []
 
 #Retrieves whole timeline
 number_tweets = 0
-for status in tweepy.Cursor(api.user_timeline, screen_name='ruchikakkad04').items():
+for status in tweepy.Cursor(api.user_timeline, screen_name='xoroohi').items():
     number_tweets = number_tweets  + 1
     alltweets.append(status._json['text'])
 print(number_tweets)
 
+#Populate all_no_rts
 for tweet in alltweets:
-    print(tweet)
+    if(tweet[:2] !=  "RT"):
+        all_no_rts.append(tweet)
 
-#Save to CSV file
-with open('tweets.csv', 'w+') as new_file:
-    csv_writer = csv.writer(new_file, delimiter = "-")
+#Sentiment Analysis
+total_polarity = 0;
+total_subj = 0;
+for tweet in all_no_rts:
+    text = TextBlob(tweet)
+    total_polarity += text.sentiment.polarity
+    total_subj += text.sentiment.subjectivity
+polarity = total_polarity/len(alltweets)
+subjectivity = total_subj/len(alltweets)
 
-    for tweet in alltweets:
-        row = [tweet.encode('utf-8')]
-        csv_writer.writerow([tweet])
+print(len(all_no_rts))
+
+print("Average Polarity: " + str(polarity))
+print("Average Subjectivity: " + str(subjectivity))
+
+# #Save to CSV file
+# with open('tweets.csv', 'w+') as new_file:
+#     csv_writer = csv.writer(new_file, delimiter = "-")
+#
+#     for tweet in alltweets:
+#         row = [tweet.encode('utf-8')]
+#         csv_writer.writerow([tweet])
 
 
 
